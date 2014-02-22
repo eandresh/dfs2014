7da60f6404c7f63ff1f9178300615bac4b3f4052
Jhon Cruz <jhon.cruz@skaphe.com>
2014-02-22 12:16:07 -0500

    jacruzca

---
+++ jacruzca/jacruzca_dart_angular/web/jacruzca_dart_angular.dart
@@ -0,0 +1,26 @@
+import 'dart:html';
+
+InputElement masa;
+InputElement estatura;
+InputElement resultado;
+ButtonElement calculateButton;
+
+void main() {
+  
+  masa = querySelector("#masa");
+  estatura = querySelector("#estatura");
+  resultado = querySelector("#resultado");
+  calculateButton = querySelector("#calculate");
+  
+  print("Buuuuuuuuu");
+  
+  calculateButton.onClick.listen(calculate);
+  
+}
+
+void calculate(Event e){
+  double masaVal = double.parse(masa.value);
+  double estaturaVal = double.parse(estatura.value);
+  double res = (masaVal/(estaturaVal*estaturaVal));
+  resultado.value = res.toString();
+}
